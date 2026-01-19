058df8ff3e9f9b091c5922155c276ad386eadd6c
Chandan Sain <chandan240107@gmail.com>
2026-01-19 10:55:30 +0530

    Update variables.py

--- basics/variables.py
+++ basics/variables.py
@@ -1,5 +1,12 @@
+# Variables practice
+
 name = "Chandan"
 age = 20
+course = "BS in Data Science"
+college = "IIT Madras"
 
 print("Name:", name)
 print("Age:", age)
+print("Course:", course)
+print("College:", college)
+
